8e9bba6fed5acc1502c4ad6003cda365699d1389
Berenika Ahmetaj <ahmetajberenika@gmail.com>
2022-01-28 19:11:49 -0500

    Completed the Article test suite

--- src/components/Article.test.js
+++ src/components/Article.test.js
@@ -10,25 +10,72 @@ import Article from "./Article";
 const testArticle = {
   id: "aMqwd",
   headline: "headline",
+  author: "Suzan Young",
+  createdOn: "2021-08-09T18:02:38-04:00 ",
+  summary: "summary",
+  body: "",
+};
+const testArticleWithoutAuthor = {
+  id: "aMqwd",
+  headline: "headline",
   createdOn: "2021-08-09T18:02:38-04:00 ",
   summary: "summary",
   body: "",
 };
 
 test("renders component without errors", () => {
-  render(<Article article={testArticle} />);
-  screen.debug();
+  const handleDelete = jest.fn();
+  const handleEditSelect = jest.fn();
+  render(
+    <Article
+      article={testArticle}
+      handleEditSelect={handleEditSelect}
+      handleDelete={handleDelete}
+    />
+  );
 });
 
-test("renders headline, author from the article when passed in through props", async () => {
-  //   render(<Article article={4} />);
-  //   const headline = await screen.findByTestId("headline");
-  //   expect(headline).toBeInTheDocument();
+test("renders headline, author from the article when passed in through props", () => {
+  const handleDelete = jest.fn();
+  const handleEditSelect = jest.fn();
+  render(
+    <Article
+      article={testArticle}
+      handleEditSelect={handleEditSelect}
+      handleDelete={handleDelete}
+    />
+  );
+
+  const headline = screen.queryByTestId(/headline/i);
+  const author = screen.queryByTestId(/author/i);
+
+  expect(headline).toContainHTML("headline");
+  expect(author).toContainHTML("Suzan Young");
 });
 
-test('renders "Associated Press" when no author is given', () => {});
+test('renders "Associated Press" when no author is given', () => {
+  const handleDelete = jest.fn();
+  const handleEditSelect = jest.fn();
+  render(
+    <Article
+      article={testArticleWithoutAuthor}
+      handleEditSelect={handleEditSelect}
+      handleDelete={handleDelete}
+    />
+  );
 
-test("executes handleDelete when the delete button is pressed", () => {});
+  const author = screen.queryByTestId(/author/i);
+  expect(author).toContainHTML("By Associated Press");
+});
+
+test("executes handleDelete when the delete button is pressed", () => {
+  const handleDelete = jest.fn();
+  render(<Article article={testArticle} handleDelete={handleDelete} />);
+
+  const deleteButton = screen.queryByTestId(/deleteButton/i);
+  userEvent.click(deleteButton);
+  expect(handleDelete).toBeCalled();
+});
 
 //Task List:
 //1. Complete all above tests. Create test article data when needed.
